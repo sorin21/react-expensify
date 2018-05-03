9b5b9e160505a12589795191ae67194048e6812b
Sorinel Dragomir <sorinel.dragomir@hub.vauban.ro>
2018-05-03 16:01:43 +0300

    Install momentjs and airbnb

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import {SingleDatePicker} from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css'
+
+// const date = new Date();  
+const now = moment();
+console.log(now.format('Do MMM, YYYY'));
 
 class ExpenseForm extends Component {
   state = {
-    description: ''
+    description: '',
+    note: '',
+    amount: ''
   };
 
   onDescriptionChange = (event) => {
     const description = event.target.value;
-    this.setState(() => {
+    this.setState(() => ({
       description: description
-    })
+    }))
+  }
+
+  onNoteChange = (event) => {
+    const note = event.target.value;
+    this.setState(() => ({
+      note: note
+    }))
+  }
+
+  onAmountChange = (event) => {
+    const amount = event.target.value;
+    // starts witd decimals
+    // then we have a .
+    // then we have only 2 decimals in the end
+    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+      this.setState(() => ({
+        amount: amount
+      }))
+    }
   }
 
   render() {
@@ -20,13 +48,19 @@ class ExpenseForm extends Component {
             type="text" 
             placeholder="Description" 
             autoFocus
+            // the value is = to current state value
             value={this.state.description}
             onChange={this.onDescriptionChange} />
           <input
+            className="amount"
             type="number"
-            placeholder="Amount" />
+            placeholder="Amount"
+            value={this.state.amount}
+            onChange={this.onAmountChange} />
           <textarea
             placeholder="Add a note for your expense(optional)"
+            value={this.state.note}
+            onChange={this.onNoteChange}
             ></textarea>
           <button>Add Expense</button>
         </form>
